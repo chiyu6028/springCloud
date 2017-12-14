3ec087b6d4f5673eefc4f2cfdabdca6969ffe83e
chiyu6028 <chiyu6028@126.com>
2017-12-14 16:03:15 +0800

    Initial commit

---
+++ service-feign/src/main/java/com/xiaolugoo/service/SchedualServiceHi.java
@@ -0,0 +1,16 @@
+package com.xiaolugoo.service;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by chinaD on 2017/12/14.
+ */
+@FeignClient(value = "service-hi")
+public interface SchedualServiceHi {
+
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
